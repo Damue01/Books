981d89dcd405556817a875c075d18328420ff941
damue <damue@semigraph.com>
2025-06-06 19:32:43 +0800

    备份1

--- MakeLua/LUAGO/go/ch03/src/luago/main.go
+++ MakeLua/LUAGO/go/ch03/src/luago/main.go
@@ -1,14 +1,21 @@
 package main
 
-import "fmt"
-import "io/ioutil"
-import "os"
-import "luago/binchunk"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/zxh0/lua.go/binchunk"
+
+	. "lua.go/vm" // Importing the vm package to use Instruction type and its methods
+)
 
 func main() {
 	if len(os.Args) > 1 {
 		data, err := ioutil.ReadFile(os.Args[1])
-		if err != nil {	panic(err)	}
+		if err != nil {
+			panic(err)
+		}
 		proto := binchunk.Undump(data)
 		list(proto)
 	}
@@ -23,13 +30,17 @@ func list(f *binchunk.Prototype) {
 	}
 }
 
-func printHeader (f *binchunk.Prototype) {
+func printHeader(f *binchunk.Prototype) {
 	funcType := "main"
-	if f.LineDefined > 0 { funcType = "function" }
+	if f.LineDefined > 0 {
+		funcType = "function"
+	}
 
 	varargFlag := ""
 
-	if f.IsVararg > 0 { varargFlag = "+" }
+	if f.IsVararg > 0 {
+		varargFlag = "+"
+	}
 
 	fmt.Printf("\n%s <%s:%d,%d> (%d instruction)\n", funcType,
 		f.Source, f.LineDefined, f.LastLineDefined, len(f.Code))
@@ -41,17 +52,20 @@ func printHeader (f *binchunk.Prototype) {
 		len(f.LocVars), len(f.Constants), len(f.Protos))
 }
 
-func printCode (f *binchunk.Prototype) {
+func printCode(f *binchunk.Prototype) {
 	for pc, c := range f.Code {
 		line := "-"
 		if len(f.LineInfo) > 0 {
 			line = fmt.Sprintf("%d", f.LineInfo[pc])
 		}
-		fmt.Printf("\t%d\t[%s]\t0x%08X\n", pc+1, line, c)
+		i := Instruction(c)
+		fmt.Printf("\t%d\t[%s]\t%s \t", pc+1, line, i.OpName())
+		printOperands(i)
+		fmt.Printf("\n")
 	}
 }
 
-func printDetail (f *binchunk.Prototype) {
+func printDetail(f *binchunk.Prototype) {
 	fmt.Printf("constants (%d):\n", len(f.Constants))
 	for i, k := range f.Constants {
 		fmt.Printf("\t%d\t%s\n", i+1, constantToString(k))
@@ -70,21 +84,62 @@ func printDetail (f *binchunk.Prototype) {
 	}
 }
 
-func constantToString (k interface{}) string {
+func constantToString(k interface{}) string {
 	switch k.(type) {
-		case nil: return "nil"
-		case bool: return fmt.Sprintf("%t", k)
-		case float64: return fmt.Sprintf("%g", k)
-		case int64: return fmt.Sprintf("%d", k)
-		case string: return fmt.Sprintf("%q", k)
-		default: return "?"
+	case nil:
+		return "nil"
+	case bool:
+		return fmt.Sprintf("%t", k)
+	case float64:
+		return fmt.Sprintf("%g", k)
+	case int64:
+		return fmt.Sprintf("%d", k)
+	case string:
+		return fmt.Sprintf("%q", k)
+	default:
+		return "?"
 	}
 }
 
-func upvalName (f *binchunk.Prototype, idx int) string {
+func upvalName(f *binchunk.Prototype, idx int) string {
 	if len(f.UpvalueNames) > 0 {
 		return f.UpvalueNames[idx]
 	}
 	return "-"
 }
 
+func printOperands(i Instruction) {
+	switch i.OpMode() {
+	case IABC:
+		a, b, c := i.ABC()
+		fmt.Printf("%d", a)
+		if i.BMode() != OpArgN {
+			if b > 0xFF {
+				fmt.Printf("%d", -1-b&0xFF)
+			} else {
+				fmt.Printf("%d", b)
+			}
+		}
+		if i.CMode() != OpArgN {
+			if c > 0xFF {
+				fmt.Printf("%d", -1-c&0xFF)
+			} else {
+				fmt.Printf("%d", c)
+			}
+		}
+	case IABx:
+		a, bx := i.ABx()
+		fmt.Printf("%d", a)
+		if i.BMode() == OpArgK {
+			fmt.Printf("%d", -1-bx)
+		} else if i.BMode() == OpArgU {
+			fmt.Printf("%d", bx)
+		}
+	case IAsBx:
+		a, sbx := i.AsBx()
+		fmt.Printf("%d %d", a, sbx)
+	case IAx:
+		ax := i.Ax()
+		fmt.Printf("%d", -l-ax)
+	}
+}
